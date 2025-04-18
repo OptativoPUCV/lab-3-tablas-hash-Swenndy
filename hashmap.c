817185527b3d39c590d5bc590328490e0e6e0b61
Your Name <you@example.com>
2025-04-18 15:52:59 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -5,74 +5,101 @@
 #include <ctype.h>
 #include "hashmap.h"
 
-
 typedef struct HashMap HashMap;
-int enlarge_called=0;
+int enlarge_called = 0;
 
-struct HashMap {
-    Pair ** buckets;
-    long size; //cantidad de datos/pairs en la tabla
-    long capacity; //capacidad de la tabla
-    long current; //indice del ultimo dato accedido
+struct HashMap
+{
+    Pair **buckets;
+    long size;     // cantidad de datos/pairs en la tabla
+    long capacity; // capacidad de la tabla
+    long current;  // indice del ultimo dato accedido
 };
 
-Pair * createPair( char * key,  void * value) {
-    Pair * new = (Pair *)malloc(sizeof(Pair));
+Pair *createPair(char *key, void *value)
+{
+    Pair *new = (Pair *)malloc(sizeof(Pair));
     new->key = key;
     new->value = value;
     return new;
 }
 
-long hash( char * key, long capacity) {
+long hash(char *key, long capacity)
+{
     unsigned long hash = 0;
-     char * ptr;
-    for (ptr = key; *ptr != '\0'; ptr++) {
-        hash += hash*32 + tolower(*ptr);
+    char *ptr;
+    for (ptr = key; *ptr != '\0'; ptr++)
+    {
+        hash += hash * 32 + tolower(*ptr);
     }
-    return hash%capacity;
+    return hash % capacity;
 }
 
-int is_equal(void* key1, void* key2){
-    if(key1==NULL || key2==NULL) return 0;
-    if(strcmp((char*)key1,(char*)key2) == 0) return 1;
+int is_equal(void *key1, void *key2)
+{
+    if (key1 == NULL || key2 == NULL)
+        return 0;
+    if (strcmp((char *)key1, (char *)key2) == 0)
+        return 1;
     return 0;
 }
 
-
-void insertMap(HashMap * map, char * key, void * value) {
-
-
+void insertMap(HashMap *map, char *key, void *value)
+{
+    int pos = hash(key, map->capacity);
+    Pair *new = createPair(key, value);
+    if (map->buckets[pos] == NULL)
+    {
+        map->buckets[pos] = new;
+        map->size++;
+    }
+    else
+    {
+        while (map->buckets[pos] != NULL)
+        {
+            if (is_equal(map->buckets[pos]->key, key))
+            {
+                map->buckets[pos]->value = value;
+                free(new);
+                return;
+            }
+            pos++;
+            pos = pos % map->capacity;
+        }
+        map->buckets[pos] = new;
+        map->size++;
+    }
 }
 
-void enlarge(HashMap * map) {
-    enlarge_called = 1; //no borrar (testing purposes)
-
-
+void enlarge(HashMap *map)
+{
+    enlarge_called = 1; // no borrar (testing purposes)
 }
 
-
-HashMap * createMap(long capacity) {
+HashMap *createMap(long capacity)
+{
 
     return NULL;
 }
 
-void eraseMap(HashMap * map,  char * key) {    
-
-
+void eraseMap(HashMap *map, char *key)
+{
 }
 
-Pair * searchMap(HashMap * map,  char * key) {   
-
+Pair *searchMap(HashMap *map, char *key)
+{
 
     return NULL;
 }
 
-Pair * firstMap(HashMap * map) {
+Pair *firstMap(HashMap *map)
+{
 
     return NULL;
 }
 
-Pair * nextMap(HashMap * map) {
+Pair *nextMap(HashMap *map)
+{
 
     return NULL;
 }
